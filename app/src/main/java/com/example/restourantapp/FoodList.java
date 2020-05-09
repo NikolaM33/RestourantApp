9fa1b28f6c2a1475482242a23eb6d7c97c61aa07
NikolaM33 <milenkovicnikola33@yahoo.com>
2020-05-09 22:23:17 +0200

    Added Functionality,Restaurant list,Food List and Food Details

--- app/src/main/java/com/example/restourantapp/FoodList.java
+++ app/src/main/java/com/example/restourantapp/FoodList.java
@@ -1,14 +1,83 @@
 package com.example.restourantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.restourantapp.Interface.ItemClickListener;
+import com.example.restourantapp.Model.Food;
+import com.example.restourantapp.view.FoodViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class FoodList extends AppCompatActivity {
+    private static final String TAG = "FOODlIST";
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private FirebaseDatabase database;
+    private DatabaseReference foodList;
+    private String restaurantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
+
+        //firebase
+
+        database = FirebaseDatabase.getInstance();
+        foodList = database.getReference("Restourant");
+
+        recyclerView = findViewById(R.id.recycler_food);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        //get Intent here
+        if (getIntent() != null) {
+            restaurantName = getIntent().getStringExtra("RestaurantName");
+            if (!restaurantName.isEmpty() && restaurantName != null) {
+                Log.i(TAG, "onCreate: " + restaurantName);
+                loadListFood(restaurantName);
+            }
+
+        }
+
+
+    }
+
+    private void loadListFood(final String restaurantName) {
+        adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(Food.class, R.layout.food_item, FoodViewHolder.class, foodList.child(restaurantName).child("Food")) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder foodViewHolder, Food food, int i) {
+                foodViewHolder.foodName.setText(food.getName());
+                Log.i(TAG, "populateViewHolder: " + food.getName());
+                Picasso.with(getBaseContext()).load(food.getImage()).into(foodViewHolder.foodImage);
+                final Food local = food;
+                foodViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(FoodList.this, "" + local.getName(), Toast.LENGTH_SHORT).show();
+                        Intent foodDetails = new Intent(getApplicationContext(), FoodDetails.class);
+                        foodDetails.putExtra("FoodName", local.getName());
+                        foodDetails.putExtra("RestaurantID", restaurantName);
+                        Log.i(TAG, "onClick: " + local.getName());
+                        startActivity(foodDetails);
+
+                    }
+                });
+
+            }
+        };
+        recyclerView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/restourantapp/Model/User.java
@@ -0,0 +1,62 @@
+package com.example.restourantapp.Model;
+
+public class User {
+    private String Name;
+    private String Password;
+    private String Email;
+    private String PhoneNumber;
+    private String LastName;
+
+    public User() {
+
+
+    }
+
+    public User(String name, String password, String email, String phoneNumber, String lastName) {
+        Name = name;
+        Password = password;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        LastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.PhoneNumber = phoneNumber;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
